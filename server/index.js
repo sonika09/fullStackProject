a826e4bff041b5e0802b386746379cc6b9a7f599
sonika <sonikasoni0517@gmail.com>
2025-01-31 14:56:12 +0530

    all done

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ require("./models/User");
 require("./services/passport");
 const authRoutes = require("./routes/authRoutes");
 const keys = require("./config/keys");
-const cookie = require("cookie-session");
 const cookieSession = require("cookie-session");
 
 const app = express();
@@ -26,12 +25,3 @@ authRoutes(app);
 const PORT = process.env.PORT || 5000;
 console.log(PORT);
 app.listen(PORT);
-
-// http://localhost:5000/
-
-// process.env.PORT - when we run our application on production otherwise in development mode we use by default 5000
-
-//https://fullstackproject-1-yalc.onrender.com/ - backend live for this url
-// sonikasoni0517 - username mongodb
-// M7sb0hPKwR4WF6hV - password
-//connection string - mongodb+srv://sonikasoni0517:<db_password>@cluster0.cyghq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
